90d0a75e0973fbe5b680f716007167e384a830ee
Verma-Sushaant <152786175+Verma-Sushaant@users.noreply.github.com>
2026-03-18 11:23:23 +0530

    Template for "apis.h" header file

---
+++ Firmware/Pesticide_System/apis.h
@@ -0,0 +1,7 @@
+// Wifi config
+#define WIFI_SSID "YOUR WIFI_SSID"
+#define WIFI_PASSWORD "YOUR WIFI_PASSWORD"
+
+// Database config
+#define API_KEY "YOUR API_KEY"
+#define DATABASE_URL "YOUR DATABASE_URL"
