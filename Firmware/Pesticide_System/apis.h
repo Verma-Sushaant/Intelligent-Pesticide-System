737a41ee579175ec34358f4069448d7ac506d838
Verma-Sushaant <152786175+Verma-Sushaant@users.noreply.github.com>
2026-03-18 11:20:25 +0530

    Deleting sensitive file

--- Firmware/Pesticide_System/apis.h
+++
@@ -1,7 +0,0 @@
-// Wifi config
-#define WIFI_SSID "x200"//"Balaji Ground floor 4G""levi"
-#define WIFI_PASSWORD "12307927"//"Balaji@786""74499495"
-
-// Firebase config
-#define API_KEY "AIzaSyAxF0LWrko29uBF0QgZy4NmHoYr-qyf3I4"
-#define DATABASE_URL "https://intelligent-pesticide-system-default-rtdb.asia-southeast1.firebasedatabase.app/"
